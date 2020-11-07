cd346152146d6442d1a3701998b30476a0dbfd06
florian-leica <florian.leica@develon.com>
2020-11-07 10:08:38 +0100

    Fixed default title

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,7 @@ export default {
   */
   head: {
     titleTemplate: '%s — Global Group Consulting',
-    title: process.env.npm_package_name || '',
+    title: 'Web App',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
