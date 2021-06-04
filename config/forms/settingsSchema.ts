ee6192f6020c717339d23670b10b567a51621e83
Leica Florian <florian.leica@gmail.com>
2021-06-04 18:11:54 +0200

    Adds settings entry for clubRequestrNotifyEmail

--- config/forms/settingsSchema.ts
+++ config/forms/settingsSchema.ts
@@ -25,6 +25,13 @@ export function globalSettings(context: Vue): FormSchema[] {
           min: "5:00"
         }
       }
-    }
+    },
+    {
+      cols: {
+        clubRequestNotifyEmail: {
+          label: "filters-club-request-notify-email"
+        }
+      }
+    },
   ]
 }
