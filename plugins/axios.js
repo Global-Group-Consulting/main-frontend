c21a9c48c58e46968475fe8a2a4677a08cc2c414
leicaflorian <florian.leica@gmail.com>
2021-07-07 12:19:07 +0200

    Minor changes

--- plugins/axios.js
+++ plugins/axios.js
@@ -4,7 +4,7 @@ export default function ({ $axios, redirect }) {
   })
 
   $axios.onError(error => {
-    const code = parseInt(error.response && error.response.status)
+    //const code = parseInt(error.response && error.response.status)
 
     /* if (code === 400) {
       redirect('/400')
