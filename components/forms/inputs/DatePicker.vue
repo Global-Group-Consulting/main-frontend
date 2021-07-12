e2c774795cf1b4f6e86a82bd0e0ec6015774a99b
leicaflorian <florian.leica@gmail.com>
2021-07-12 17:45:58 +0200

    Updated Date component

--- components/forms/inputs/DatePicker.vue
+++ components/forms/inputs/DatePicker.vue
@@ -1,7 +1,7 @@
 <template>
   <v-menu
     ref="menu"
-    v-model="opened"
+    v-model="menuOpened"
     :close-on-content-click="false"
     :nudge-right="40"
     transition="scale-transition"
@@ -11,6 +11,7 @@
   >
     <template v-slot:activator="{ on }">
       <v-text-field
+        ref="textField"
         :value="dateValue | dateFormatter"
         :label="label"
         :prepend-icon="
@@ -22,6 +23,7 @@
         v-on="on"
         v-bind="$attrs"
         @change="onInput"
+        @click:clear="onClear"
         :clearable="clearable"
         :disabled="disabled"
         :class="{ 'edit-mode': editMode }"
@@ -31,19 +33,20 @@
         </template>
 
         <template v-slot:label>
+
           <slot name="label"></slot>
         </template>
       </v-text-field>
     </template>
+
     <v-date-picker
       ref="picker"
-      :type="type"
       v-model="dateValue"
       @input="onInput"
       :readonly="readonly"
       :disabled="disabled"
-      :picker-date="dateValue ? '' : initialDate | datePickerFormatter"
-      :min="min | datePickerFormatter"
+      :picker-date="pickerDate"
+      :min="minDate"
       :max="max"
       locale="it"
     >
@@ -51,64 +54,90 @@
   </v-menu>
 </template>
 
-<script>
+
+<script lang="ts">
+import {Component, Prop, Vue, Watch} from "vue-property-decorator";
 import {datePickerFormatter} from "~/plugins/filters";
 
-export default {
-  name: "DatePicker",
-  data() {
-    return {
-      opened: false,
-      dateValue: datePickerFormatter(this.value)
-    };
-  },
-  props: {
-    value: "",
-    label: "",
-    initialDate: "",
-    min: "",
-    max: "",
-    startByYear: {
-      type: Boolean,
-      default: true
-    },
-    readonly: Boolean,
-    disabled: Boolean,
-    editMode: Boolean,
-    type: {
-      type: String,
-      default: null,
-    }
-  },
-  computed: {
-    clearable() {
-      return this.$attrs.clearable ?? (!this.readonly && !this.disabled)
-    }
-  },
-  methods: {
-    onInput(value) {
-      if (this.opened) {
-        this.opened = false;
-      }
+@Component
+export default class DatePicker extends Vue {
+  @Prop({type: String})
+  value!: string
+  @Prop({type: String})
+  label!: string
+  @Prop({type: String})
+  initialDate!: string
+  @Prop({type: String})
+  min!: string
+  @Prop({type: String})
+  max!: string
+  @Prop({type: Boolean, default: true})
+  startByYear!: boolean
+  @Prop({type: Boolean})
+  readonly!: boolean
+  @Prop({type: Boolean})
+  disabled!: boolean
+  @Prop({type: Boolean})
+  editMode!: boolean
+  @Prop({type: String, default: "date"})
+  type!: string
 
-      this.$emit("change", value);
-    }
-  },
-  watch: {
-    value: function (value) {
-      const newValue = datePickerFormatter(this.value);
+  $refs!: {
+    picker: any,
+    textField: HTMLInputElement
+  }
 
-      if (this.dateValue !== newValue) {
-        this.dateValue = newValue;
-      }
-    },
-    opened(val) {
-      if (this.startByYear) {
-        val && setTimeout(() => (this.$refs.picker.activePicker = "YEAR"));
-      }
+  menuOpened: boolean = false
+  dateValue: string = this.value ? datePickerFormatter(this.value) : null
+
+  get clearable() {
+    return this.$attrs.clearable ?? (!this.readonly && !this.disabled)
+  }
+
+  get pickerDate() {
+    return this.dateValue ? '' : datePickerFormatter(this.initialDate)
+  }
+
+  get minDate() {
+    return datePickerFormatter(this.min)
+  }
+
+  onInput(value: string) {
+    if (this.menuOpened) {
+      this.menuOpened = false;
+    }
+
+    if (this.dateValue !== value) {
+      this.dateValue = value
+    }
+
+    this.$emit("change", value);
+  }
+
+  onClear(ev: any) {
+    this.dateValue = null
+
+    setTimeout(() => {
+      this.$refs.textField.blur();
+    })
+
+    this.$emit("change", null);
+  }
+
+  @Watch("value")
+  onValueChange(value: string) {
+    const newValue = datePickerFormatter(value);
+
+    if (this.dateValue !== newValue) {
+      this.dateValue = newValue;
     }
   }
-};
-</script>
 
-<style scoped></style>
+  @Watch("menuOpened")
+  onOpenedChange(val: string) {
+    if (this.startByYear) {
+      val && setTimeout(() => (this.$refs.picker.activePicker = "YEAR"));
+    }
+  }
+}
+</script>
