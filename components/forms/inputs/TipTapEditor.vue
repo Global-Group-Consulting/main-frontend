e11322de21c35ba3322cceffc7cdda5c23263f90
leicaflorian <florian.leica@gmail.com>
2021-08-20 13:05:41 +0200

    Started feature

---
+++ components/forms/inputs/TipTapEditor.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+
+@Component
+export default class TipTapEditor extends Vue {
+
+}
+</script>
