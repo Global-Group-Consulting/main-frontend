ae72e96af4a0606612f46254f6028e7c21785776
leicaflorian <florian.leica@gmail.com>
2023-03-02 15:46:27 +0100

    Fixes Magazine card

--- components/dashboard/MagazineCard.vue
+++ components/dashboard/MagazineCard.vue
@@ -1,12 +1,19 @@
 <template>
-  <v-card>
+  <v-card class="h-100" :loading="downloading">
+    <template v-slot:progress>
+      <v-progress-linear
+          indeterminate
+      ></v-progress-linear>
+    </template>
+
     <v-card-text class="d-flex">
       <v-skeleton-loader
-          :loading="loading"
+          :loading="!coverFileUrl"
           width="100%"
           :height="skeletonHeight"
           type="image"
           style="width: 100%"
+          v-if="!error"
       >
         <a href="#" @click.prevent="downloadFile" target="_blank" class="d-flex">
           <v-img
@@ -14,66 +21,91 @@
               :width="width"
               contain
               class=""
+              @error="onImgError"
           ></v-img>
         </a>
       </v-skeleton-loader>
+
+      <v-alert v-else type="error"
+               outlined tile class="mb-0">
+        Ci dispiace ma sembra ci sia stato un errore!
+      </v-alert>
 
     </v-card-text>
   </v-card>
 </template>
 
-
 <script lang="ts">
-import { Component, Prop, Vue, Watch } from 'vue-property-decorator'
+import { computed, defineComponent, onMounted, ref, watch } from '@vue/composition-api'
 import { IMagazine } from '~/@types/Magazine'
+import { useFileDownloader } from '~/composables/fileDownloader'
 
-@Component
-export default class MagazineCard extends Vue {
-  @Prop({ type: String, default: '100%' })
-  width!: string
-  @Prop({ type: String })
-  skeletonHeight!: string
-  coverFileUrl: string = ''
-  loading = true
+export default defineComponent({
+  name: 'MagazineCard',
+  props: {
+    width: {
+      type: String,
+      default: '100%'
+    },
+    skeletonHeight: {
+      type: String
+    }
+  },
+  setup (props, { root }) {
+    const { $store, $fileUtils, $alerts, $apiCalls } = root
+    const fileDownloader = useFileDownloader($alerts)
+    const coverFileUrl = ref('')
+    const loading = ref(true)
+    const downloading = ref(false)
+    const error = ref()
+    const currentMagazine = computed((): IMagazine => {
+      return $store.getters['magazine/currentMagazine']
+    })
 
-  get currentMagazine (): IMagazine {
-    return this.$store.getters['magazine/currentMagazine']
-  }
+    async function downloadFile () {
+      if (downloading.value) {
+        return
+      }
 
-  downloadFile () {
-    try {
-      this.$fileUtils.download(this.currentMagazine.fileId, this.currentMagazine.title + '.pdf')
-    } catch (e) {
-      this.$alerts.error(e)
+      downloading.value = true
+
+      try {
+        await fileDownloader.download(() => $apiCalls.files.download(currentMagazine.value.fileId),
+            currentMagazine.value.title + '.pdf',
+            fileDownloader.types.PDF)
+      } catch (e) {
+        $alerts.error(e)
+      }
+
+      downloading.value = false
+    }
+
+    function onImgError (e: ErrorEvent) {
+      error.value = e
+    }
+
+    watch(() => currentMagazine.value, async (value) => {
+      if (value && value.coverFileId) {
+        coverFileUrl.value = fileDownloader.srcUrl(value.coverFileId)
+      }
+    }, { immediate: true })
+
+    onMounted(() => {
+      $store.dispatch('magazine/fetchCurrent')
+    })
+
+    return {
+      coverFileUrl,
+      loading,
+      downloading,
+      error,
+      downloadFile,
+      onImgError
     }
   }
-
-  @Watch('currentMagazine', { deep: true })
-  async onCurrentMagazineChange (value: IMagazine) {
-    const file = value.coverFile
-
-    if (!file) {
-      return
-    }
-
-    try {
-      const result = await this.$apiCalls.files.download(file._id)
-
-      //const fileName = file.clientName
-      const mimeType = `${file.type}/${file.subtype}`
-      //const fileData = result.data
-
-      this.coverFileUrl = URL.createObjectURL(new Blob([result.data], { type: mimeType }))
-
-      this.loading = false
-    } catch (er) {
-      this.loading = false
-      this.$alerts.error(er)
-    }
-  }
-
-  mounted () {
-    this.$store.dispatch('magazine/fetchCurrent')
-  }
-}
+})
 </script>
+
+<style scoped>
+
+</style>
