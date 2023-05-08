8452a88973bcc0ae36b03e6165e34ea5eea53187
leicaflorian <florian.leica@gmail.com>
2023-05-08 14:49:53 +0200

    Add ability to format enum select text

--- classes/BasicEnum.ts
+++ classes/BasicEnum.ts
@@ -1,3 +1,5 @@
+import { SelectOption } from '~/@types/components/SelectInput'
+
 export interface EnumData {
   id: string,
   index?: number | string,
@@ -11,25 +13,28 @@ export interface EnumData {
   reqRoles?: number[]
 }
 
-
 class BasicEnum {
   private enumName: string
   protected data: Record<any, EnumData> = {}
-
-  constructor(enumName: string) {
+  
+  constructor (enumName: string) {
     this.enumName = enumName
   }
-
-  get(id: any): EnumData {
+  
+  formatListText (value: any, data: EnumData): string {
+    return value
+  }
+  
+  get (id: any): EnumData {
     if (typeof id === 'number' || !isNaN(Number(id))) {
       return {
         index: id,
         ...this.data[id]
       }
     }
-
+    
     let foundedEnum: EnumData = {
-      id: ""
+      id: ''
     }
 
     Object.entries(this.data).forEach(function (entry) {
@@ -62,14 +67,14 @@ class BasicEnum {
   }
 
 
-  get list(): { value: number | string, text: string }[] {
+  get list(): SelectOption[] {
     return Object.entries(this.data).reduce<{ value: number | string, text: string }[]>((acc, curr) => {
       const value = +curr[0]
-
+      
       if (!curr[1].hidden) {
         acc.push({
           value: Number.isNaN(value) ? curr[0] : value,
-          text: curr[1].id
+          text: this.formatListText(curr[1].id, curr[1])
         })
       }
 
